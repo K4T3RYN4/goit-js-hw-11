f4b703f2e5f4f166cdb412d5f70d2077e3bc9c79
kate <kateryna1morozova@gmail.com>
2025-10-20 20:12:07 +0300

    finish api query logic

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 export function getImagesByQuery(query) {
     return axios.get("https://pixabay.com/api/", {
         params: {
@@ -12,6 +15,12 @@ export function getImagesByQuery(query) {
     })
         .then(res => {
             console.log(query)
+            if (res.data.hits.length === 0) {
+                return iziToast.error({
+                    position: 'topRight',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                });
+            };
             console.log(res.data.hits)
             return res.data.hits
         })
